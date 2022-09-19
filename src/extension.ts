6b64f4dab47a256f52af3feb017f2292fb8499df
Sandro Roth [LU14843] <sandro.roth@lukb.ch>
2022-09-19 12:13:17 +0200

    Init project

---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "extension.createTranslation",
+    async () => {
+      vscode.window.showInformationMessage('Hello World!');
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
